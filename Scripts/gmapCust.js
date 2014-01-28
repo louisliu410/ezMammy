f09905a68d4001be50d1a1678475bb63ce35be08
louisliu410 <louisliu410@gmail.com>
2014-01-28 09:13:27 +0800

    first

---
+++ Scripts/gmapCust.js
@@ -0,0 +1,30 @@
+﻿
+function LongPress(map, length) {
+    this.length_ = length;
+    var me = this;
+    me.map_ = map;
+    me.timeoutId_ = null;
+    google.maps.event.addListener(map, 'mousedown', function (e) {
+        me.onMouseDown_(e);
+    });
+    google.maps.event.addListener(map, 'mouseup', function (e) {
+        me.onMouseUp_(e);
+    });
+    google.maps.event.addListener(map, 'drag', function (e) {
+        me.onMapDrag_(e);
+    });
+};
+LongPress.prototype.onMouseUp_ = function (e) {
+    clearTimeout(this.timeoutId_);
+};
+LongPress.prototype.onMouseDown_ = function (e) {
+    clearTimeout(this.timeoutId_);
+    var map = this.map_;
+    var event = e;
+    this.timeoutId_ = setTimeout(function () {
+        google.maps.event.trigger(map, 'longpress', event);
+    }, this.length_);
+};
+LongPress.prototype.onMapDrag_ = function (e) {
+    clearTimeout(this.timeoutId_);
+};
